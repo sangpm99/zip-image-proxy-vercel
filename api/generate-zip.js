9f600b90443506f3a2c0867b92e8695ece7d5610
sangpm99 <sangpm72@wru.vn>
2025-06-19 17:28:27 +0700

    Updated

--- api/generate-zip.js
+++ api/generate-zip.js
@@ -2,6 +2,14 @@ const axios = require("axios");
 const JSZip = require("jszip");
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // CORS preflight
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
   }
